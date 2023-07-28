6befcfac8a8b1101a7c58b0d58c71bfe4f613b79
noelfoka <nomgnenoel@gmail.com>
2023-07-28 20:14:02 +0100

    learn about casting

---
+++ casting.py
@@ -0,0 +1,3 @@
+x = str(3)  # x will be "3"
+y = int(3)  # y will be 3
+z = float(3)  # z will be 3.0
---
+++ variable.py
@@ -0,0 +1,7 @@
+# Variable
+# Python has no command for declaring a variable.
+x = 32
+y = "Joel FOKA"
+
+print(x)
+print(y)
