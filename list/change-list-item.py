e0e667edad6d255bd11c6b72c7e553a37abbd50e
noelfoka <nomgnenoel@gmail.com>
2023-07-29 07:13:28 +0100

    learn how to change list items

---
+++ list/change-list-item.py
@@ -0,0 +1,14 @@
+thisList = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+
+# change the value of "banana" and "cherry" with "blackcurrant" and "watermelon"
+thisList[1:3] = ["blackcurrant", "watermelon"]
+print(thisList)
+
+# Change the second value by replacing it with two new values
+myList = ["apple", "banana", "cherry"]
+myList[1:2] = ["blackcurrant", "watermelon"]
+print(myList)
+
+# Inset items
+myList.insert(2, "Orange")
+print(myList)
