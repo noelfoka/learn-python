ca745886e5e7d347ac1a7a524fa6b57d0dd21055
noelfoka <nomgnenoel@gmail.com>
2023-07-29 08:40:48 +0100

    learn how to remove item 1

---
+++ list/remove-list-item.py
@@ -0,0 +1,15 @@
+# List items
+myList = ["apple", "banana", "orang", "cherry"]
+
+# Remove "banana"
+myList.remove("banana")
+print(myList)
+
+# Remove the second item
+myList.pop(1)
+print(myList)
+
+# Remove the last item
+
+myList.pop()
+print(myList)
