4930ee4d15714a795b36ebc79a84044bd29a6b27
noelfoka <nomgnenoel@gmail.com>
2023-07-29 05:53:45 +0100

    create list folder

---
+++ list.py
@@ -0,0 +1,16 @@
+# List
+myList = ["Apple", "Bananas", "Mango"]
+print(myList)
+
+# list length
+print(len(myList))
+
+# List items and data type
+
+list1 = ["Apple", "Bananas", "Cherry"]
+list2 = [1, 2, 3, 4, 5]
+list3 = [True, False, False]
+
+print(type(myList))
+print(type(list1))
+print(type(list2))
