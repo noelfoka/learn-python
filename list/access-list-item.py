c26d15456dbb34f3a77f74e5a8728025e76d3619
noelfoka <nomgnenoel@gmail.com>
2023-07-29 06:50:03 +0100

    learn how to access to item list

---
+++ list/access-list-item.py
@@ -0,0 +1,17 @@
+thisList = ["Bananas", "Apple", "Cherry"]
+
+# Access to the first item
+print(thisList[0])
+
+# print the last item
+print(thisList[-1])
+
+# Range of index
+myList = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(myList[2:5])
+print(myList[:4])
+
+# Check if the items exist
+fruitList = ["Mango", "Bananas", "Potatos", "Orange"]
+if "Bananas" in fruitList:
+    print("Bananas is in fruit list")
