100bbcaa0e596ba7d8d8acd052b2a71f8117badf
noelfoka <nomgnenoel@gmail.com>
2023-07-29 08:03:21 +0100

    learn how to add list items

---
+++ list/add-list-item.py
@@ -0,0 +1,18 @@
+# Add the item to the end of the list
+myList = ["apple", "banana", "cherry"]
+myList.append("Orange")
+print(myList)
+
+# Insert an item as the second position
+myList.insert(1, "watermelon")
+print(myList)
+
+# Extend lists
+thisList = [
+    "potato",
+    "igname",
+    "cocoyam",
+]
+
+myList.extend(thisList)
+print(myList)
