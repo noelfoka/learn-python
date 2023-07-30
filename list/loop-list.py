8d7058bcbdbbf6527da68fc5c951985d4aff3bf6
noelfoka <nomgnenoel@gmail.com>
2023-07-30 04:21:25 +0100

    learn how to use for loop

---
+++ list/loop-list.py
@@ -0,0 +1,13 @@
+# Loop through the list
+thisList = ["appla", "banana", "cherry"]
+for x in thisList:
+    print(x)
+
+# Loop through the next number
+myList = [
+    "potato",
+    "mango",
+    "cocoyam",
+]
+for i in range(len(myList)):
+    print(myList[i])
