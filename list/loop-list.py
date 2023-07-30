603496aed4ca73e1ebe33d9fa244f57511468701
noelfoka <nomgnenoel@gmail.com>
2023-07-30 04:27:46 +0100

    learn how to use while loop

--- list/loop-list.py
+++ list/loop-list.py
@@ -11,3 +11,15 @@ myList = [
 ]
 for i in range(len(myList)):
     print(myList[i])
+
+# Using while loop
+myList = ["apple", "banana", "cherry", "mango", "potatos"]
+
+i = 0
+while i < len(myList):
+    print(myList[i])
+    i = i + 1
+
+# Looping using list comprehension
+myList = ["apple", "banana", "cherry", "mango"]
+[print(x) for x in myList]
